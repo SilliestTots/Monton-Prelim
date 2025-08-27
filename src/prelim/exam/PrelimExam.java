a22d0e2cc82c3fc0e5f0ee9e7d1b51f06be22508
SilliestTots <elizahmonton@gmail.com>
2025-08-27 16:23:59 +0800

    prelimmm ragh

--- src/prelim/exam/PrelimExam.java
+++ src/prelim/exam/PrelimExam.java
@@ -1,20 +1,20 @@
 
 package prelim.exam;
-
+    import java.util.Scanner;
 public class PrelimExam {
-     import java.util.Scanner;
+     
      
     public static void main(String[] args) {
          Scanner scan = new Scanner (System.in);
         
         System.out.print("Enter Principal Amount: ");
-        double pa = scan.nextfloat();
-        System.out.print("Enter Annual Interest Rate: ");
-        double air = scan.nextfloat();
+        double pa = scan.nextFloat();
+        System.out.print("Enter Annual Interest Rate(%): ");
+        double air = scan.nextFloat();
         
         double interest = pa*air/100;
         
-        System.out.print("Interest for one year " + interest);
+        System.out.print("Interest for one year: " + interest);
     }
     
 }
